d4d093094cbec2a8612a94d101c65c9ae73bca09
nicoriver9 <114373470+nicoriver9@users.noreply.github.com>
2022-12-15 17:39:14 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,5 @@
 const db = require("mysql2-promise")();
 
-// database = {
-//   host     : "sql811.main-hosting.eu",
-//   database : "u748790910_Credentials",
-//   user     : "u748790910_Credentials",
-//   password : "Credentials123",
-//   port:      3306
-// }
 
 database = {
   host     : "localhost",
